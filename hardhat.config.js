24bb5c12776b2745c4fd3d4fe02c9587f34072fd
harsh08hc <136369712+harsh08hc@users.noreply.github.com>
2023-07-18 00:33:28 +0530

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,18 +2,13 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: '0.8.0',
   networks: {
-    // Define your network settings here
-    // For example, you can use the local Hardhat network for development
-    hardhat: {},
-    // Or connect to a public network like Ethereum Mainnet
-    // mainnet: {
-    //   url: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
-    //   accounts: [PRIVATE_KEY],
-    // },
+    hardhat: {
+      chainId: 1337,
+    },
   },
 };
